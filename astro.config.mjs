72d307d63685013b5e8ae7223ccbb6930505bb22
Paltita715 <manuel715pl@gmail.com>
2024-12-15 20:20:43 -0500

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 
 import react from "@astrojs/react"
+import vercelAdapter from "@astrojs/vercel"
 
-import vercel from "@astrojs/vercel/serverless"
+// import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
-  adapter: vercel(),
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true },
-  // }),
+  output: "server",
+  adapter: vercelAdapter(),
 })
